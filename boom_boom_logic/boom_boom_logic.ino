6312f858f86e16ee0abb58475db3f50d289d10e6
sokolovnatal <74265853+sokolovnatal@users.noreply.github.com>
2023-03-07 10:24:41 -0500

    test

--- boom_boom_logic/boom_boom_logic.ino
+++ boom_boom_logic/boom_boom_logic.ino
@@ -12,3 +12,11 @@
 #include <SPI.h>
 #include <SD.h>
 
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+
+  }
