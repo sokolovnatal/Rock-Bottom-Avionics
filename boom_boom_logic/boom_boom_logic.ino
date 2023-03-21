54d1ee3b2999abfd32aec1e229c9200bedfa60a8
JanosBanocziRuof <67029836+JanosBanocziRuof@users.noreply.github.com>
2023-03-21 17:28:41 -0400

    Organized and fixed some stuff

--- boom_boom_logic/boom_boom_logic.ino
+++ boom_boom_logic/boom_boom_logic.ino
@@ -11,59 +11,72 @@
 
 File avionicsFile;
 
-  //gloabls
-String dataLabel1 = "";//fill in for column names as needed
-String dataLabel2 = "";
-bool label = true;
+// globals
+String DATALABEL1 = "";  // fill in for column names as needed
+String DATALABEL2 = "";
+bool LABEL = true;
+
+// functions
+bool SDInit();
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  
-
-  if(SD.begin()){
-    Serial.println("SD card is ready to use");    
-  }else{
-    Serial.println("SD card initialization failed");
-  }
-
-  avionicsFile = SD.create("");//Figure out how to create multiple files with different names, talk to avionics people
-
-  if(avionicsFile){
-    while(avionicsFile.avaliable()){
-      Serial.write(avionicsFile.read());
-    }
-    avionicsFile.close();
-  }else{
-    Serial.println("error opening file");
-  }    
-  
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    // Wait for serial port to connect. Needed for native USB port only
+  }
 
+  SDInit();
+
+  avionicsFile = SD.open("test_1.txt", FILE_WRITE);  // Figure out how to create multiple files with different names, talk to avionics people
+  if (avionicsFile) {
+    Serial.println("File open.");
+    avionicsFile.println("Ahemmm... Testing 1 2 3 testing");
+    avionicsFile.close();
+    Serial.println("File closed.");
+  } else {
+    Serial.println("Error opening file.");
   }
 
 
+}
 
-void loop(){
-	
-    //print out column headers
-    
-    if (avionicsFile) {    
-      while(label){ //runs once
-        Serial.print(dataLabel1);
-        Serial.print(" , ");//fill in label
-        Serial.println(dataLabel2);
-        label=false;        
-      }
-        avionicsFile.print();
-        avionicsFile.print(",");    //print commas for the amount of columns
-        avionicsFile.println();
-        avionicsFile.close(); // close the file
-  }else {
+
+
+void loop() {
+
+
+  // Im confuzzled what this code is meant to do. So I have ignored it ;-;
+  // Print out column headers
+
+  /*if (avionicsFile) {
+    while (LABEL) {  //runs once
+      Serial.print(DATALABEL1);
+      Serial.print(" , ");  //fill in label
+      Serial.println(DATALABEL2);
+      LABEL = false;
+    }
+    avionicsFile.print();
+    avionicsFile.print(",");  // Print commas for the amount of columns
+    avionicsFile.println();
+    avionicsFile.close();  // Close the file
+  } else {
     Serial.println("error opening test.txt");
   }
-  delay(3000);//stores data every __ seconds (currently 3)
+  delay(3000);  //stores data every __ seconds (currently 3)*/
 }
-  
-//need to sort into functions ;-;
+
+
+
+
+
+// functions
+
+bool SDInit() {
+  if (SD.begin(BUILTIN_SDCARD)) {
+    Serial.println("SD card initialized");
+  } else {
+    Serial.println("SD card initialization failed.\nHalting code");
+    exit(0);
+  }
+}
