d1242505791b6a75c922d67181bec443f0a324ad
sokolovnatal <74265853+sokolovnatal@users.noreply.github.com>
2023-03-24 17:00:06 -0400

    Attempting ethernet

--- boom_boom_logic/boom_boom_logic.ino
+++ boom_boom_logic/boom_boom_logic.ino
@@ -8,6 +8,8 @@
 #include <Adafruit_AHTX0.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
 
 File avionicsFile;
 
@@ -22,6 +24,22 @@ const int analogAccelXPin = 23;
 const int analogAccelYPin = 22;
 const int analogAccelZPin = 21;
 
+const int pin = 10;//Configuring ethernet pin
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+
+// Enter a MAC address and IP address laptop below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED//change to laptop
+};
+IPAddress ip(192, 168, 1, 177); //assign static ip address
+char serverName[] = "web.rockbottom.net"; //  test web page server
+
+unsigned int localPort = 8888;      // local port to listen on
+
 // functions
 bool SDInit();
 void fileNamePicker();
@@ -30,6 +48,22 @@ bool storeData(String, double);  // Appends timestamp, data type, and data to CS
 void measureAndStoreBigAccel();  // Grabs data from the board, and then passes it to the storeData function
 
 void setup() {
+  Ethernet.init(pin);
+  Ethernet.begin(mac, ip);
+    // Check for Ethernet hardware present
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+    while (true) {
+      delay(1); // do nothing, no point running without Ethernet hardware
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Ethernet cable is not connected.");
+  }
+
+  // start UDP
+  Udp.begin(localPort);
+}
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -42,6 +76,7 @@ void setup() {
   // testing
   measureAndStoreBigAccel();
   measureAndStoreBigAccel();
+
 }
 
 
